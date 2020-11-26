a119ac98667091b3d9464ec2bc6375f23c421f0b
Tainah Boarini Bernardo <tainahboarini@gmail.com>
2020-11-26 10:49:20 -0300

    requisição de imagens e nomes dos pokemons prontas na homme

--- pokedex/src/global/GlobalState.js
+++ pokedex/src/global/GlobalState.js
@@ -4,7 +4,7 @@ import { BASE_URL } from "../constants/urls";
 import GlobalStateContext from "./GlobalStateContext";
 
 const GlobalState = (props) => {
-    const [pokemon, setPokemon] = useState({})
+    const [pokemonHome, setPokemonHome] = useState([])
 
     useEffect(()=>{
         getArrayPokemons()     
@@ -13,40 +13,29 @@ const GlobalState = (props) => {
 //requisição de API
 const getArrayPokemons = () => {
 
-    const listPokemon = [];
-
-    for (let i = 1; i<20; i++) {
-
         axios
-        .get(`${BASE_URL}/${i}/`)
+        .get(`${BASE_URL}`)
         .then((response)=>{
-            console.log("SPRITES",response)
-            listPokemon[i - 1] = response.data
-
-            console.log(listPokemon.length)
-            if (listPokemon.length === 19) {
-                setPokemon(listPokemon)
-            }
+            console.log("Axios response",response)
+            setPokemonHome(response.data.results)
+          
         })
         .catch((error)=>{
             console.log(error)
-        })
-    }    
+        })  
 }
 
-const states = {pokemon}
-const setters = {setPokemon}
+const states = {pokemonHome}
+const setters = {setPokemonHome}
 const requests = {getArrayPokemons}
 
 const data = {states, setters, requests}
 
 return (
     <div>
-        {console.log("TESTE",requests)}
         <GlobalStateContext.Provider value={data}>
             {props.children}   
-            {getArrayPokemons}   
-            {console.log("return do globalState", pokemon)}  
+            {console.log("return do globalState", pokemonHome)}  
         </GlobalStateContext.Provider>
     </div>
     
